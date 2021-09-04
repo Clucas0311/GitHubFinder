1408fb86706042cf3a41be44fe716b04928ac56a
Clucas0311 <clucas0311@gmail.com>
2021-09-04 17:23:50 -0400

    Created state and components for some files

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React, { Component } from 'react'
-import NavBar from './components/layout/NavBar'
-import './App.css'
+import React, { Component } from "react";
+import NavBar from "./components/layout/NavBar";
+import Users from "./components/users/Users";
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [],
+      loading: false,
+    };
+  }
+  // Runs when the component first renders
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const { data } = await axios.get("https://api.github.com/users");
+    this.setState({ users: data, loading: false });
+  }
+
   render() {
     return (
-      // React.Fragment makes invisible divs 
-      <React.Fragment className='App'>
-        <NavBar title='GitHub Finder' icon='fab fa-github'/>
-       
-      </React.Fragment>
-    )
+      // React.Fragment makes invisible divs
+      <div className="App">
+        <NavBar title="GitHub Finder" icon="fab fa-github" />
+        <div className="container">
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
+      </div>
+    );
   }
 }
 
-export default App
+export default App;
--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,27 +1,25 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-export class NavBar extends Component {
-    // We use defualtProps when as a way of having props incase props were not passed in
-    static defaultProps = {
-        title :'Github Finder',
-        icon : 'fab fa-github'
-    }
-    // PropTypes are used as a type checker
-    static propTypes = {
-        title: PropTypes.string.isRequired, 
-        icon: PropTypes.string.isRequired
-    }
-    render() {
-        const {title, icon} = this.props
-        return (
-            <nav className="navbar bg-primary">
-                <h1>
-                    <i className={icon} /> {title}
-                </h1>
-            </nav>
-        )
-    }
-}
+const NavBar = (props) => {
+  const { title, icon } = props;
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+    </nav>
+  );
+};
+// We use defualtProps when as a way of having props incase props were not passed in
+NavBar.defaultProps = {
+  title: "Github Finder",
+  icon: "fab fa-github",
+};
+// PropTypes are used as a type checker
+NavBar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
 
-export default NavBar
+export default NavBar;
