a98b8061be4ed34135c697723aa4fd2b8160a574
Clucas0311 <clucas0311@gmail.com>
2021-09-11 15:22:49 -0400

    Converted Components to Hooks

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,68 +1,63 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-class Search extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: "",
-    };
-  }
-  // Used proptypes to make these props available if proptypes wasn't declared
-  static propTypes = {
-    searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired,
-    setAlert: PropTypes.func.isRequired,
-  };
+const Search = ({ searchUsers, clearUsers, setAlert, showClear }) => {
+  // how to use useState destructure out the state and state with its current state and the set state and its initial value
+  const [text, setText] = useState("");
 
-  onSubmit = (e) => {
+  const onSubmit = (e) => {
     // On submits need e.prevent default to stop submitting/reloading page --> always use this with onSubmit
     e.preventDefault();
     // this sets an alert when the user doesn't input anything
-    if (this.state.text === "") {
-      this.props.setAlert("Please enter something", "light");
+    if (text === "") {
+      setAlert("Please enter something", "light");
     } else {
       // Passing this prop up to App Component
-      this.props.searchUsers(this.state.text);
+      searchUsers(text);
       // This will clear our state, clearing out the text bar after submitting
-      this.setState({ text: "" });
+      setText("");
     }
   };
   // In order to use the text field we must use the onchange function this will render the component and allow you to write in the text field
   // e.target.name allows us to use onchange in multiple fields
   // Arrow function allows us not use bind
-  onChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+  const onChange = (e) => {
+    setText(e.target.value);
   };
-  render() {
-    const { showClear, clearUsers } = this.props;
-    return (
-      <div>
-        <form onSubmit={this.onSubmit} className="form">
-          <input
-            type="text"
-            name="text"
-            placeholder="Search Users..."
-            value={this.state.text}
-            // Allows us to write
-            onChange={this.onChange}
-          />
-          <input
-            type="submit"
-            value="Search"
-            className="btn btn-dark btn-block"
-          />
-        </form>
-        {/*This shows the clear button on when showClear is we have users */}
-        {showClear && (
-          <button className="btn btn-light btn-block" onClick={clearUsers}>
-            Clear
-          </button>
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <form onSubmit={onSubmit} className="form">
+        <input
+          type="text"
+          name="text"
+          placeholder="Search Users..."
+          value={text}
+          // Allows us to write
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value="Search"
+          className="btn btn-dark btn-block"
+        />
+      </form>
+      {/*This shows the clear button on when showClear is we have users */}
+      {showClear && (
+        <button className="btn btn-light btn-block" onClick={clearUsers}>
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
+
+// Used proptypes to make these props available if proptypes wasn't declared
+// Proptypes go underneath the function for functional components
+Search.propTypes = {
+  searchUsers: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired,
+  setAlert: PropTypes.func.isRequired,
+};
 
 export default Search;
